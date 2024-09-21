7c68b7cdb70e9440afbf2b03a566d94ceb3c813a
Russel Aniel <anielrussel13@gmail.com>
2024-09-21 17:22:07 +0800

    Fetch products  using RTKQuery.

--- src/app/(pages)/layout.js
+++ src/app/(pages)/layout.js
@@ -1,13 +1,19 @@
+"use client";
+
 import React from "react";
 import Navbar from "../../components/page/Navbar";
 import Footer from "../../components/page/Footer";
+import { Provider } from "react-redux";
+import { store } from "@/stores/store";
 
 export default function PagesLayout({ children }) {
   return (
     <main>
-      <Navbar />
-      {children}
-      <Footer />
+      <Provider store={store}>
+        <Navbar />
+        {children}
+        <Footer />
+      </Provider>
     </main>
   );
 }
--- src/components/page/HPTopSelling.js
+++ src/components/page/HPTopSelling.js
@@ -1,55 +1,46 @@
+"use client";
+
 import React from "react";
 
 import ProductCard from "./ProductCard";
 
-import sleeve from "../../../public/sleeve.png";
 import { Button } from "@/components/ui/button";
+import { Skeleton } from "@/components/ui/skeleton";
 
-const products = [
-  {
-    image: sleeve,
-    name: "SC Black sleeve",
-    rating: 5,
-    price: "$120",
-  },
-  {
-    image: sleeve,
-    name: "SC Black sleeve",
-    rating: 5,
-    price: "$120",
-    prevPrice: "$260",
-    discount: "-20%",
-  },
-  {
-    image: sleeve,
-    name: "SC Black sleeve",
-    rating: 5,
-    price: "$120",
-  },
-  {
-    image: sleeve,
-    name: "SC Black sleeve",
-    rating: 5,
-    price: "$120",
-  },
-];
+import { useGetAllProductsByCategoryQuery } from "@/stores/apiSlice";
 
 const HPTopSelling = () => {
+  const { data: womensDresses, isLoading: isLoadingWomensDresses } =
+    useGetAllProductsByCategoryQuery("womens-dresses");
   return (
     <div className="h-auto flex flex-col items-center py-20 md:px-20 px-5 gap-16">
       <h1 className="text-[48px]">TOP SELLING</h1>
       <section className="flex md:justify-between gap-4 md:gap-0 w-full overflow-x-auto">
-        {products.map((product) => (
-          <ProductCard
-            key={product.name}
-            src={product.image}
-            name={product.name}
-            rating={product.rating}
-            price={product.price}
-            prevPrice={product?.prevPrice}
-            discount={product?.discount}
-          />
-        ))}
+        {isLoadingWomensDresses ? ( // Display skeletons while loading
+          Array.from({ length: 4 }).map((_, index) => (
+            <div key={index} className="space-y-2">
+              <Skeleton className="md:min-w-[295px] md:min-h-[298px] min-w-[172px] min-h-[174px] rounded-xl" />
+              <div className="space-y-2">
+                <Skeleton className="h-4 md:min-w-[295px] min-w-[172px]" />
+                <Skeleton className="h-4 md:min-w-[295px] min-w-[172px]" />
+              </div>
+            </div>
+          ))
+        ) : (
+          <>
+            {womensDresses?.products.slice(0, 4).map((product) => (
+              <ProductCard
+                key={product.id}
+                src={product?.images[0]}
+                name={product.title}
+                rating={product?.rating}
+                price={product.price}
+                prevPrice={product?.prevPrice}
+                discount={product?.discountPercentage}
+              />
+            ))}
+          </>
+        )}
       </section>
       <Button variant="outline" className="rounded-full w-full md:w-1/5 py-6">
         View All
